7c98e5f28849bd0024c1c43f218238a5c9f77571
Luiz Velho <lvelho@impa.br>
2013-01-08 20:16:03 -0200

    Initial Commit

---
+++ nn/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  nn
+//
+//  Created by Luiz Velho on 1/8/13.
+//  Copyright (c) 2013 Luiz Velho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
